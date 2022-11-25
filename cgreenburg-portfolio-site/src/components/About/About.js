941b2c01ece50ab5a0b0062b1d7bcf8f234a2a27
relero90 <relero90@gmail.com>
2022-11-25 14:42:53 -0700

    created about component with filler image and text

--- cgreenburg-portfolio-site/src/components/About/About.js
+++ cgreenburg-portfolio-site/src/components/About/About.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "./About.css";
+import photo from "./about-photo.png";
 
 function About() {
-  return <div className="placeholder">About Candace Placeholder</div>;
+  return (
+    <div className="aboutContainer">
+      <img
+        src={photo}
+        alt="Candace with her granddaughter"
+        className="aboutImg"
+      />
+      <p className="aboutText">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa enim quo
+        corrupti iure, ut, fuga blanditiis eius maiores dolorem tempora dicta
+        exercitationem perspiciatis cupiditate, quia omnis necessitatibus.
+        Sequi, quasi, quod alias suscipit recusandae blanditiis mollitia odit
+        debitis atque nostrum ipsum reiciendis praesentium officiis modi laborum
+        minus laboriosam doloremque, explicabo dolore!
+      </p>
+    </div>
+  );
 }
 
 export default About;
